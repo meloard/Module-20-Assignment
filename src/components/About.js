e31b67c62097c68cb8faf6cc04a1769fe8fdc7bc
Josh Adam <joshsabag@gmail.com>
2022-11-22 21:19:56 -0500

    react app running

---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+            Well Hello!
+            <br className="hidden lg:inline-block" />
+            I'm Josh and I am an aspiring fullstack web-developer!
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            After creating my first company at 22, I am excited to refocus my endeavors with an emphasis on tech development and digital platforms! I believe in creating businesses that are morally and socially responsible, structured with an emphasis on e-commerce
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contact
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./PersonalPic.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: "Password Generator App",
+    subtitle: "CSS - Javascript",
+    description:
+      "Front-End password creator",
+    image: "./PasswordGenerator.jpg",
+    link: "https://github.com/meloard/Module-3-Assignment",
+  },
+  {
+    title: "Javascript Quiz App",
+    subtitle: "CSS - Javascript",
+    description:
+      "Quiz on Javascript fundamentals, complete with active timer and scoring with local storage",
+    image: "./QuizApp.jpg",
+    link: "https://github.com/meloard/Module-4-Assignment",
+  },
+  {
+    title: "Planner/Scheduler App",
+    subtitle: "Javascript - DOM",
+    description:
+      "Work Day scheduler that color coordinates times that have passed, current, and upcoming",
+    image: "./Scheduler.jpg",
+    link: "https://github.com/meloard/Module-5-Assignment",
+  },
+  {
+    title: "Habitation HotSpot",
+    subtitle: "Javascript - Google API",
+    description:
+      "Created App that allows user to search an address, finding different local attractions based on search criteria",
+    image: "./HabitationHotSpot.jpg",
+    link: "https://github.com/Zoocko42/skrongproject1",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "CSS",
+  "HTML",
+  "Node",
+  "GraphQL",
+  "MYSQL",
+  "NOSQL",
+  "MongoDB",
+  "Express",
+];
